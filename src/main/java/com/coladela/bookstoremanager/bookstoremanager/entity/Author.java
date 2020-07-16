42a8993f97299bab7e38cc64d68a44fb9055ff7b
coladela <rmcoladela@gmail.com>
2020-07-16 12:43:56 -0300

    chore: auto deploy test

---
+++ src/main/java/com/coladela/bookstoremanager/bookstoremanager/entity/Author.java
@@ -0,0 +1,14 @@
+package com.coladela.bookstoremanager.bookstoremanager.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Author {
+    //@Id
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    //private Long id;
+    //private string name;
+
+}
