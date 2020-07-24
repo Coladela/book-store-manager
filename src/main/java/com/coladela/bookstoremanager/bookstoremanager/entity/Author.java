01afe3c1cfec795d974adda5e163fa39fa1a25d2
coladela <rmcoladela@gmail.com>
2020-07-24 15:52:19 -0300

    setup lombok and poco creation

--- src/main/java/com/coladela/bookstoremanager/bookstoremanager/entity/Author.java
+++ src/main/java/com/coladela/bookstoremanager/bookstoremanager/entity/Author.java
@@ -1,14 +1,25 @@
 package com.coladela.bookstoremanager.bookstoremanager.entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Author {
-    //@Id
-    //@GeneratedValue(strategy = GenerationType.IDENTITY)
-    //private Long id;
-    //private string name;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
 
 }
